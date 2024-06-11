0ba5ad719fb9f13442f9ec31cfd8cc666490adeb
eldarcodes <prog.eldar@gmail.com>
2024-06-11 17:02:05 +0300

    feat(auth): add auth actions in marketing page

--- components/marketing/heading.tsx
+++ components/marketing/heading.tsx
@@ -1,9 +1,16 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
 
 export function Heading() {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl">
@@ -16,9 +23,29 @@ export function Heading() {
         happens.
       </h3>
 
-      <Button>
-        Enter Plano <ArrowRight className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Plano
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button size="sm">
+            Get Plano free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 }
