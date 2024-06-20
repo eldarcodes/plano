6d335016ddf1838bbb739cf9e343b7d15691ea24
eldarcodes <prog.eldar@gmail.com>
2024-06-20 14:51:43 +0300

    feat(ui): update toast config

--- providers/toast-provider.tsx
+++ providers/toast-provider.tsx
@@ -3,5 +3,5 @@
 import { Toaster } from "@/components/ui/sonner";
 
 export function ToastProvider() {
-  return <Toaster position="bottom-center" duration={2500} />;
+  return <Toaster richColors position="bottom-center" duration={2500} />;
 }
