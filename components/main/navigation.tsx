6ebbe90529c12f288d90fbfcb273955c176b0d67
eldarcodes <prog.eldar@gmail.com>
2024-06-20 16:49:13 +0300

    fix(ui): remove z-index classes

--- components/main/navigation.tsx
+++ components/main/navigation.tsx
@@ -132,7 +132,7 @@ export function Navigation() {
       <aside
         ref={sidebarRef}
         className={cn(
-          "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[9999]",
+          "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col",
           isResetting && "transition-all ease-in-out duration-300",
           isMobile && "w-0"
         )}
@@ -183,7 +183,7 @@ export function Navigation() {
       <div
         ref={navbarRef}
         className={cn(
-          "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
+          "absolute top-0 left-60 w-[calc(100%-240px)]",
           isResetting && "transition-all ease-in-out duration-300",
           isMobile && "left-0 w-full"
         )}
